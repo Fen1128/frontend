00f7a5b29d8df715190aa475edf79d138428a862
Fen1128 <stevendotcom2805@gmail.com>
2024-07-10 15:23:01 +0700

    saya membuuat example album

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,175 @@
-import Kotak1 from "./components/Kotak1"
-import Kotak2 from "./components/Kotak2"
-
+import Kotak1 from "./components/Kotak1";
+import Kotak2 from "./components/Kotak2";
 
 function App() {
-
   return (
     <>
-      <h1>hello!</h1>
-      <Kotak1/>
-      <Kotak2/>
+      <div className="container-fluid bg-dark p-2">
+        <div className="container text-white">Album</div>
+      </div>
+
+      <div className="container text-center mt-5 width bg-white">
+        <h3>Album Example</h3>
+        <p>
+          Something short album and save your all memories. but not too <br />{" "}
+          short so foks don't simply skip over it entertly
+        </p>
+        <button type="button" class="btn btn-primary m-3">
+          Main call to action
+        </button>
+        <button type="button" class="btn btn-secondary">
+          Secondary action
+        </button>
+
+        <div className="row">
+          <div class="card col m-3">
+            <img src="https://wallpapercave.com/wp/wp3429906.png" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <div className="d-flex">
+                <div className="btn btn-primary m-1">View</div>
+                <div className="btn btn-primary m-1">Edit</div>
+                <div className="ms-auto">ms 9</div>
+              </div>
+            </div>
+          </div>
+          <div class="card col m-3">
+            <img src="https://wallpapercave.com/wp/wp3429906.png" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <div className="d-flex">
+                <div className="btn btn-primary m-1">View</div>
+                <div className="btn btn-primary m-1">Edit</div>
+                <div className="ms-auto">ms 9</div>
+              </div>
+            </div>
+          </div>
+          <div class="card col m-3">
+            <img src="https://wallpapercave.com/wp/wp3429906.png" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <div className="d-flex">
+                <div className="btn btn-primary m-1">View</div>
+                <div className="btn btn-primary m-1">Edit</div>
+                <div className="ms-auto">ms 9</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div class="card col m-3">
+            <img src="https://wallpapercave.com/wp/wp3429906.png" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <div className="d-flex">
+                <div className="btn btn-primary m-1">View</div>
+                <div className="btn btn-primary m-1">Edit</div>
+                <div className="ms-auto">ms 9</div>
+              </div>
+            </div>
+          </div>
+          <div class="card col m-3">
+            <img src="https://wallpapercave.com/wp/wp3429906.png" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <div className="d-flex">
+                <div className="btn btn-primary m-1">View</div>
+                <div className="btn btn-primary m-1">Edit</div>
+                <div className="ms-auto">ms 9</div>
+              </div>
+            </div>
+          </div>
+          <div class="card col m-3">
+            <img src="https://wallpapercave.com/wp/wp3429906.png" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <div className="d-flex">
+                <div className="btn btn-primary m-1">View</div>
+                <div className="btn btn-primary m-1">Edit</div>
+                <div className="ms-auto">ms 9</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div class="card col m-3">
+            <img src="https://wallpapercave.com/wp/wp3429906.png" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <div className="d-flex">
+                <div className="btn btn-primary m-1">View</div>
+                <div className="btn btn-primary m-1">Edit</div>
+                <div className="ms-auto">ms 9</div>
+              </div>
+            </div>
+          </div>
+          <div class="card col m-3">
+            <img src="https://wallpapercave.com/wp/wp3429906.png" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <div className="d-flex">
+                <div className="btn btn-primary m-1">View</div>
+                <div className="btn btn-primary m-1">Edit</div>
+                <div className="ms-auto">ms 9</div>
+              </div>
+            </div>
+          </div>
+          <div class="card col m-3">
+            <img src="https://wallpapercave.com/wp/wp3429906.png" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <div className="d-flex">
+                <div className="btn btn-primary m-1">View</div>
+                <div className="btn btn-primary m-1">Edit</div>
+                <div className="ms-auto">ms 9</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="container-fluid bg-light p-2">
+          <div className="container">Album example is C Bootstrap, but please download and customice it yourself <br/> 
+          New to Bootstrap? visit the homepage or read our getting started guide</div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
